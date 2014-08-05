79f598d98a640c70c0ced018217c27575b24a9dc
Elanchezhiyan Elango <eelango@riverbed.com>
2014-08-05 16:52:16 -0700

    Removed flymake and added flycheck

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
         auto-complete
         yasnippet
         python-pep8
+        flycheck
         )
       )
 
@@ -14,37 +15,7 @@
 
 (add-to-list 'load-path "~/.emacs.d")
 
-(setq python-check-command "pyflakes")
-
-;;flymake
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-               'flymake-create-temp-inplace))
-       (local-file (file-relative-name
-            temp-file
-            (file-name-directory buffer-file-name))))
-      (list "pycheckers"  (list local-file))))
-   (add-to-list 'flymake-allowed-file-name-masks
-             '("\\.py\\'" flymake-pyflakes-init)))
-(load-library "flymake-cursor")
-(global-set-key [f10] 'flymake-goto-prev-error)
-(global-set-key [f11] 'flymake-goto-next-error)
-
-;;(custom-set-faces
-;; '(flymake-errline ((((class color)) (:underline "red"))))
-;; '(flymake-warnline ((((class color)) (:underline "yellow")))))
-
-;;ropemacs
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport 't)
-
-;;; bind RET to py-newline-and-indent
-(add-hook 'python-mode-hook '(lambda ()
-                   (define-key python-mode-map "\C-m" 'newline-and-indent)))
-
+;; Tab spacing related settings
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq-default py-indent-offset 4)
@@ -67,8 +38,17 @@
 ;;python-pep8
 (require 'python-pep8)
 
-;;python-pylint
-;;(require 'python-pylint)
+;; enable flycheck
+(require 'flycheck)
+;; Enable flycheck globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+;; Extend flake8 checker to include pylint also
+(flycheck-add-next-checker 'python-flake8 'python-pylint)
+;; Keymaps to navigate to the errors
+(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-cn" 'flycheck-next-error)))
+(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-cp" 'flycheck-previous-error)))
+;; Shortcut to select a different checker
+(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-cs" 'flycheck-select-checker)))
 
 ;;Modify the backup files directory
 (setq backup-directory-alist '(("." . "/home/eelango/.saves")))
@@ -96,3 +76,15 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
+
+
+;; Things that might be useful later.
+;; (global-set-key [f10] 'flymake-goto-prev-error)
+;; (global-set-key [f11] 'flymake-goto-next-error)
+
+;; ;; bind RET to py-newline-and-indent
+;; (add-hook 'python-mode-hook '(lambda ()
+;;                    (define-key python-mode-map "\C-m" 'newline-and-indent)))
+
+;; (setq python-check-command "pylint")
