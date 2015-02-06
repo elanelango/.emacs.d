0214a042da7e040e4da3787ca0726ddf73de1dc8
Elanchezhiyan Elango <elanelango@gmail.com>
2015-02-05 22:19:14 -0800

    Adding elscreen and multiterm stuff

--- init.el
+++ init.el
@@ -10,6 +10,8 @@
         xcscope+
         yaml-mode
         org-mode
+        multi-term
+        elscreen
         )
       )
 
@@ -50,8 +52,80 @@
 ;; I'm used to transient-mark-mode. So having it enabled.
 ;; (setq transient-mark-mode nil)
 
+;; Save and restore buffers
+(desktop-save-mode 1)
+
 ;; Shortcut for rename-buffer
+;; TODO: This doesn't work. Why?
 (define-key global-map (kbd "M-n") 'rename-buffer)
+
+;; Set bash as multi-term-program
+(setq multi-term-program "/bin/bash")
+
+;; F5 shortcut to open new multi-term
+(global-set-key (kbd "<f5>") 'multi-term)
+
+;; Set multi-term's buffer size
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq term-buffer-maximum-size 10000)))
+
+(defun term-send-C-x ()
+  "Type C-x in term-mode."
+  (interactive)
+  (term-send-raw-string "\C-x"))
+
+(defun term-send-C-s ()
+  "Type C-s in term-mode."
+  (interactive)
+  (term-send-raw-string "\C-s"))
+
+;; Shortcuts for switching between terminals
+(add-hook 'term-mode-hook
+          (lambda ()
+            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+            (add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
+            (add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
+            (add-to-list 'term-bind-key-alist '("C-c C-x" . term-send-C-x))
+            (add-to-list 'term-bind-key-alist '("C-c C-s" . term-send-C-s))))
+
+(when (require 'term nil t)
+  (defun term-handle-ansi-terminal-messages (message)
+    (while (string-match "\eAnSiT.+\n" message)
+      ;; Extract the command code and the argument.
+      (let* ((start (match-beginning 0))
+             (command-code (aref message (+ start 6)))
+             (argument
+              (save-match-data
+                (substring message
+                           (+ start 8)
+                           (string-match "\r?\n" message
+                                         (+ start 8))))))
+        ;; Delete this command from MESSAGE.
+        (setq message (replace-match "" t t message))
+
+        (cond ((= command-code ?c)
+               (setq term-ansi-at-dir argument))
+              ((= command-code ?h)
+               (setq term-ansi-at-host argument))
+              ((= command-code ?u)
+               (setq term-ansi-at-user argument))
+              ((= command-code ?e)
+               (save-excursion
+                 (find-file-other-window argument)))
+              ((= command-code ?x)
+               (save-excursion
+                 (find-file argument))))))
+
+    (when (and term-ansi-at-host term-ansi-at-dir term-ansi-at-user)
+      (setq buffer-file-name
+            (format "%s@%s:%s" term-ansi-at-user term-ansi-at-host term-ansi-at-dir))
+      (set-buffer-modified-p nil)
+      (setq default-directory (if (string= term-ansi-at-host (system-name))
+                                  (concatenate 'string term-ansi-at-dir "/")
+                                (format "/%s@%s:%s/" term-ansi-at-user term-ansi-at-host term-ansi-at-dir))))
+    message))
 
 ;;ropemacs
 (require 'pymacs)
@@ -124,3 +198,32 @@
 (global-unset-key (kbd "<M-down>"))
 
 ;; (setq python-check-command "pylint")
+
+;; Start elscreen
+(elscreen-start)
+
+;; Name of the first elscreen
+(elscreen-screen-nickname "dev")
+
+;; function that creates a elscreen, renames the screen, opens multiterm
+;; ssh to remote server and opens a screen with the specified name in the
+;; remote server.
+(defun open-remote-screen (term-name)
+  (let* ((mt-buffer-name "*terminal<1>*")
+         (term-process (get-buffer-process mt-buffer-name)))
+    (elscreen-create)
+    (elscreen-screen-nickname term-name)
+    (multi-term)
+    (comint-send-string term-process "ssh picrite\n")
+    (comint-send-string term-process (concat "screen -dr " term-name "\n"))
+    (with-current-buffer mt-buffer-name
+      (rename-buffer term-name))))
+
+;; Open a bunch of elscreens with multiterms
+(defun init-multi-terms ()
+  (interactive)
+  (open-remote-screen "picrite")
+  (open-remote-screen "mainsh250"))
+
+;; Run the init-multi-terms function on startup
+(add-hook 'emacs-startup-hook 'init-multi-terms)
